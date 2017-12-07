9a6d25032964408b9113e2524a363b710b243670
JasonEtco <jasonetco@gmail.com>
2017-12-07 14:45:56 -0500

    Add tests

--- tests/server.test.js
+++ tests/server.test.js
@@ -8,9 +8,29 @@ describe('server', () => {
     server = createServer()
   })
 
-  it('redirects from / to /TOKEN', async () => {
-    const res = await request(server).get('/')
-    expect(res.status).toBe(302)
-    expect(typeof res.headers.location).toBe('string')
+  describe('GET /', () => {
+    it('redirects from / to /TOKEN', async () => {
+      const res = await request(server).get('/')
+      expect(res.status).toBe(302)
+      expect(typeof res.headers.location).toBe('string')
+    })
+  })
+
+  describe('GET /:channel', () => {
+    it('returns the proper HTML', async () => {
+      const res = await request(server).get('/fake-channel')
+      expect(res.status).toBe(200)
+      expect(res.text).toMatchSnapshot()
+    })
+  })
+
+  it('POST /:channel/redeliver re-emits a payload', async () => {
+    const payload = {
+      foo: true,
+      bar: false
+    }
+
+    const res = await request(server).post('/jason/redeliver').send(payload)
+    expect(res.status).toBe(200)
   })
 })

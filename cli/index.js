23d7568ae9a42fcfd71bc95f6fa34086793d745b
Brandon Keepers <bkeepers@github.com>
2017-12-07 08:52:15 -0600

    WIP CLI

---
+++ cli/index.js
@@ -0,0 +1,54 @@
+const EventSource = require('eventsource')
+const superagent = require('superagent')
+
+module.exports = class Proxy {
+  constructor ({source, target}) {
+    this.source = source
+    this.target = target
+  }
+
+  onmessage (msg) {
+    const data = JSON.parse(msg.data)
+
+    const req = superagent.post(this.target).send(data.body)
+
+    delete data.body
+
+    Object.keys(data).forEach(key => {
+      req.set(key, data[key])
+    })
+
+    req.end((err, res) => {
+      console.log(err, res)
+    })
+  }
+
+  onopen () {
+    console.log('Connected', this.events.url)
+  }
+
+  onerror (err) {
+    console.error(err)
+  }
+
+  start () {
+    const events = new EventSource(this.source)
+
+    // Reconnect immediately
+    events.reconnectInterval = 0
+
+    events.addEventListener('message', this.onmessage.bind(this))
+    events.addEventListener('open', this.onopen.bind(this))
+    events.addEventListener('error', this.onerror.bind(this))
+
+    console.log(`Proxying requests from ${this.source} to ${this.target}`)
+
+    this.events = events
+
+    return events
+  }
+
+  close () {
+    this.events.close()
+  }
+}

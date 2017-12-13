7d380e307ff633076d276e6c1f86e081e016d48c
JasonEtco <jasonetco@gmail.com>
2017-12-13 02:51:21 -0500

    Write tests for EventDescription

--- src/components/EventDescription.js
+++ src/components/EventDescription.js
@@ -20,7 +20,7 @@ export default class EventDescription extends Component {
       <div className="text-gray">
         <p className="mb-0">There was a <strong>{event}</strong> event received on <code>{formattedTime}</code>.</p>
         {onARepo && <p className="mt-0">This event was sent by <strong>{payload.repository.full_name}</strong>.</p>}
-        {onRepos && <p className="mt-0">This event was triggered against: {payload.repositories.map(r => <span>{r.full_name}</span>)}.</p>}
+        {onRepos && <p className="mt-0">This event was triggered against: {payload.repositories.map(r => <span key={r.full_name}>{r.full_name}</span>)}.</p>}
       </div>
     )
   }

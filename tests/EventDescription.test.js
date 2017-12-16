719693eb1d81205af8e25d4f50ad1f0211892f96
Jason Etcovitch <jasonetco@gmail.com>
2017-12-16 12:35:41 -0500

    Cleanup test file

--- tests/EventDescription.test.js
+++ tests/EventDescription.test.js
@@ -13,25 +13,27 @@ describe('<ListItem />', () => {
     }
   })
 
-  it('renders the correct description', () => {
-    const wrapper = shallow(<EventDescription {...props} />)
-    expect(wrapper.find('p').text()).toBe('There was a issues event received on Wednesday, December 13th 2017, 2:01:14 am.')
-  })
+  describe('render', () => {
+    it('renders the correct description', () => {
+      const wrapper = shallow(<EventDescription {...props} />)
+      expect(wrapper.find('p').text()).toBe('There was a issues event received on Wednesday, December 13th 2017, 2:01:14 am.')
+    })
 
-  it('renders the correct description when on one repo', () => {
-    const payload = { repository: { full_name: 'probot/probot' } }
-    const wrapper = shallow(<EventDescription {...props} payload={payload} />)
-    expect(wrapper.children().length).toBe(2)
-    expect(wrapper.childAt(1).text()).toBe('This event was sent by probot/probot.')
-  })
+    it('renders the correct description when on one repo', () => {
+      const payload = { repository: { full_name: 'probot/probot' } }
+      const wrapper = shallow(<EventDescription {...props} payload={payload} />)
+      expect(wrapper.children().length).toBe(2)
+      expect(wrapper.childAt(1).text()).toBe('This event was sent by probot/probot.')
+    })
 
-  it('renders the correct description when on multiple repos', () => {
-    const payload = { repositories: [
-      { full_name: 'probot/probot' },
-      { full_name: 'JasonEtco/pizza' }
-    ] }
-    const wrapper = shallow(<EventDescription {...props} payload={payload} />)
-    expect(wrapper.children().length).toBe(2)
-    expect(wrapper.childAt(1).text()).toBe('This event was triggered against: probot/probotJasonEtco/pizza.')
+    it('renders the correct description when on multiple repos', () => {
+      const payload = { repositories: [
+        { full_name: 'probot/probot' },
+        { full_name: 'JasonEtco/pizza' }
+      ] }
+      const wrapper = shallow(<EventDescription {...props} payload={payload} />)
+      expect(wrapper.children().length).toBe(2)
+      expect(wrapper.childAt(1).text()).toBe('This event was triggered against: probot/probotJasonEtco/pizza.')
+    })
   })
 })

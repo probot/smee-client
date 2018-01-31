022f231621ab4a7e94700951ec7bf5239129f5e2
JasonEtco <jasonetco@gmail.com>
2018-01-30 20:07:16 -0500

    Write some more EventSource tests

--- server/tests/App.test.js
+++ server/tests/App.test.js
@@ -6,7 +6,7 @@ import issuesOpened from './fixtures/issues.opened.json'
 import issuesOpenedTwo from './fixtures/issues.opened2.json'
 
 describe('<App />', () => {
-  let localStorage, wrapper
+  let localStorage, wrapper, consoleLog
 
   beforeEach(() => {
     localStorage = {
@@ -14,18 +14,22 @@ describe('<App />', () => {
       setItem: jest.fn()
     }
 
+    const EventSource = jest.fn()
+    EventSource.CONNECTING = 0
+    EventSource.CLOSED = 2
+
     Object.defineProperties(window, {
       localStorage: {
         value: localStorage
       },
       EventSource: {
-        value: jest.fn()
+        value: EventSource
       }
     })
 
     Object.defineProperty(location, 'pathname', { value: '/CHANNEL' })
 
-    console.log = jest.fn()
+    console.log = consoleLog = jest.fn()
 
     wrapper = shallow(<App />)
   })
@@ -74,6 +78,20 @@ describe('<App />', () => {
     it('sets the connection state to false', () => {
       wrapper.instance().onerror()
       expect(wrapper.state('connection')).toBeFalsy()
+    })
+
+    it('logs to the console when the state changes to connecting', () => {
+      wrapper.instance().events.readyState = 0 // CONNECTING
+
+      wrapper.instance().onerror('error')
+      expect(consoleLog).toHaveBeenCalledWith('Reconnecting...', 'error')
+    })
+
+    it('logs to the console when the state changes to closed', () => {
+      wrapper.instance().events.readyState = 0 // CONNECTING
+
+      wrapper.instance().onerror('error')
+      expect(consoleLog).toHaveBeenCalledWith('Reinitializing', 'error')
     })
   })
 

998c58ac70e7f65960ef26d0eb7a3b819450ff0b
knrt10 <tripathi.kautilya@gmail.com>
2018-03-25 23:54:15 +0530

    validated url

--- client/index.js
+++ client/index.js
@@ -6,7 +6,13 @@ class Client {
     this.source = source
     this.target = target
     this.logger = logger
+
   }
+  function validateURL(value) {
+      var urlregex = /^(https?|ftp):\/\/([a-zA-Z0-9.-]+(:[a-zA-Z0-9.&%$-]+)*@)*((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])){3}|([a-zA-Z0-9-]+\.)*[a-zA-Z0-9-]+\.(com|edu|gov|int|mil|net|org|biz|arpa|info|name|pro|aero|coop|museum|[a-zA-Z]{2}))(:[0-9]+)*(\/($|[a-zA-Z0-9.,?'\\+&%$#=~_-]+))*$/;
+      return urlregex.test(value);
+  }
+  const url = validateURL(this.source)
 
   onmessage (msg) {
     const data = JSON.parse(msg.data)
@@ -45,8 +51,12 @@ class Client {
     events.addEventListener('message', this.onmessage.bind(this))
     events.addEventListener('open', this.onopen.bind(this))
     events.addEventListener('error', this.onerror.bind(this))
+    if (url === true) {
+      this.logger.info(`Forwarding ${this.source} to ${this.target}`)
+    } else {
+      this.logger.error('Please check your WEBHOOK_PROXY_URL')
+    }
 
-    this.logger.info(`Forwarding ${this.source} to ${this.target}`)
 
     this.events = events
 

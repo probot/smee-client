54c41aa96458d1205f6a40ce5785f6698390256e
tcbyrd <tcbyrd@github.com>
2018-06-24 17:11:18 -0400

    Add react-hotkeys and initial keyMap

--- server/src/components/App.js
+++ server/src/components/App.js
@@ -1,8 +1,29 @@
 import React, {Component} from 'react'
+import {HotKeys} from 'react-hotkeys'
 import Deliveries from './Deliveries'
+
+const keyMap = {
+  moveDown: 'j',
+  moveUp: 'k',
+  open: 'o',
+  pin: 'p',
+  redeliver: 'r'
+}
 
 export default class App extends Component {
   render () {
-    return (<Deliveries />)
+    const handlers = {
+      'moveUp': (e) => console.log('pressed up'),
+      'moveDown': (e) => console.log('pressed down'),
+      'open': (e) => console.log('pressed open'),
+      'pin': (e) => console.log('pressed pin'),
+      'redeliver': (e) => console.log('pressed redeliver')
+    }
+
+    return (
+      <HotKeys keyMap={keyMap} handlers={handlers} focused>
+        <Deliveries />
+      </HotKeys>
+    )
   }
 }

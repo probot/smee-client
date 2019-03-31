7185b80eea06cb7ae6c0cc4fd63b6057ad8fbda3
Jason Etcovitch <jasonetco@github.com>
2019-03-30 21:49:05 -0600

    Update client/index.js
    Co-Authored-By: rpetti <rob.petti@gmail.com>

--- client/index.js
+++ client/index.js
@@ -18,7 +18,7 @@ class Client {
   onmessage (msg) {
     const data = JSON.parse(msg.data)
 
-    let target = url.parse(this.target, true)
+    const target = url.parse(this.target, true)
     const mergedQuery = Object.assign(target.query, data.query)
     target.search = querystring.stringify(mergedQuery)
 

1754827e8b4653af04271a028f7a8568ce8d42ba
Jason Etcovitch <jasonetco@github.com>
2019-03-30 21:02:57 -0400

    Spike out Redis Cache class

---
+++ server/redis.js
@@ -0,0 +1,30 @@
+const Redis = require('ioredis')
+
+module.exports = class Cache {
+  constructor () {
+    this.redis = new Redis(process.env.REDIS_URL || { host: 'localhost', port: 6379 })
+  }
+
+  async setForChannel (channel, id, payload) {
+    const key = `${channel}:${id}`
+    return this.redis.set(key, JSON.stringify(payload))
+  }
+
+  async getAllForChannel (channel) {
+    return new Promise(async (resolve, reject) => {
+      const keys = []
+
+      // Create a readable stream (object mode)
+      const stream = this.redis.scanStream({ match: `${channel}:*`, count: 100 })
+      stream.on('data', (resultKeys) => {
+        // `resultKeys` is an array of strings representing key names.
+        keys.push(...resultKeys)
+      })
+
+      stream.on('end', async () => {
+        const values = await Promise.all(keys.map(async key => JSON.parse(await this.redis.get(key))))
+        return resolve(values)
+      })
+    })
+  }
+}

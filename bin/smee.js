87b05244be2065169074da1c169b9685158ed219
Enrique Mingorance Cano <emingora@redhat.com>
2022-10-18 12:06:21 +0200

    commander options

--- bin/smee.js
+++ bin/smee.js
@@ -12,17 +12,19 @@ program
   .option('-t, --target <target>', 'Full URL (including protocol and path) of the target service the events will forwarded to. Default: http://127.0.0.1:PORT/PATH')
   .option('-p, --port <n>', 'Local HTTP server port', process.env.PORT || 3000)
   .option('-P, --path <path>', 'URL path to post proxied requests to`', '/')
-  .parse(process.argv)
+  .parse()
+
+const options = program.opts()
 
 let target
-if (program.target) {
-  target = program.target
+if (options.target) {
+  target = options.target
 } else {
-  target = `http://127.0.0.1:${program.port}${program.path}`
+  target = `http://127.0.0.1:${options.port}${options.path}`
 }
 
 async function setup () {
-  let source = program.url
+  let source = options.url
 
   if (!source) {
     source = await Client.createChannel()
@@ -33,3 +35,4 @@ async function setup () {
 }
 
 setup()
+

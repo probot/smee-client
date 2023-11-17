fc3f2a6f438cf8591fd129d53e6cc5862f4c9d3d
Aras Abbasi <aras.abbasi@googlemail.com>
2023-11-17 23:08:58 +0100

    chore: resolve codeql warning in unit test (#264)
    This is a small fix for a codeql warning in the tests. We can merge this
    without a release, as we are not shipping tests with our npm packages.

--- test/index.test.ts
+++ test/index.test.ts
@@ -6,7 +6,7 @@ describe("client", () => {
   describe("createChannel", () => {
     test("returns a new channel", async () => {
       const channel = await Client.createChannel();
-      expect(channel).toMatch(/https:\/\/smee\.io\/[0-9a-zA-Z]{10,}/);
+      expect(channel).toMatch(/^https:\/\/smee\.io\/[0-9a-zA-Z]{10,}$/);
     });
 
     test("throws if could not create a new channel", async () => {

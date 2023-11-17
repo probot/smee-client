94bd698271692a15e2fd96eccf8ec24c703732f8
uzlopak <aras.abbasi@googlemail.com>
2023-11-17 19:02:53 +0100

    add linting

--- index.ts
+++ index.ts
@@ -1,15 +1,15 @@
-import validator from 'validator'
-import EventSource from 'eventsource'
-import url from 'url'
-import querystring from 'querystring'
+import validator from "validator";
+import EventSource from "eventsource";
+import url from "url";
+import querystring from "querystring";
 
-type Severity = 'info' | 'error'
+type Severity = "info" | "error";
 
 interface Options {
-  source: string
-  target: string
-  logger?: Pick<Console, Severity>
-  fetch?: any
+  source: string;
+  target: string;
+  logger?: Pick<Console, Severity>;
+  fetch?: any;
 }
 
 class Client {
@@ -19,86 +19,91 @@ class Client {
   logger: Pick<Console, Severity>;
   events!: EventSource;
 
-  constructor({ source, target, logger = console, fetch = global.fetch }: Options) {
-    this.source = source
-    this.target = target
-    this.logger = logger!
-    this.fetch = fetch
+  constructor({
+    source,
+    target,
+    logger = console,
+    fetch = global.fetch,
+  }: Options) {
+    this.source = source;
+    this.target = target;
+    this.logger = logger!;
+    this.fetch = fetch;
 
     if (!validator.isURL(this.source)) {
-      throw new Error('The provided URL is invalid.')
+      throw new Error("The provided URL is invalid.");
     }
   }
 
   static async createChannel({ fetch = global.fetch } = {}) {
-    const response = await fetch('https://smee.io/new', {
-      method: 'HEAD',
-      redirect: 'manual'
+    const response = await fetch("https://smee.io/new", {
+      method: "HEAD",
+      redirect: "manual",
     });
-    const address = response.headers.get('location')
+    const address = response.headers.get("location");
     if (!address) {
-      throw new Error('Failed to create channel')
+      throw new Error("Failed to create channel");
     }
-    return address
+    return address;
   }
 
   async onmessage(msg: any) {
-    const data = JSON.parse(msg.data)
+    const data = JSON.parse(msg.data);
 
-    const target = url.parse(this.target, true)
-    const mergedQuery = { ...target.query, ...data.query }
-    target.search = querystring.stringify(mergedQuery)
+    const target = url.parse(this.target, true);
+    const mergedQuery = { ...target.query, ...data.query };
+    target.search = querystring.stringify(mergedQuery);
 
-    delete data.query
+    delete data.query;
 
-    const body = JSON.stringify(data.body)
-    delete data.body
+    const body = JSON.stringify(data.body);
+    delete data.body;
 
-    const headers: { [key: string]: any } = {}
+    const headers: { [key: string]: any } = {};
 
-    Object.keys(data).forEach(key => {
-      headers[key] = data[key]
-    })
+    Object.keys(data).forEach((key) => {
+      headers[key] = data[key];
+    });
 
-    headers['content-length'] = Buffer.byteLength(body)
+    headers["content-length"] = Buffer.byteLength(body);
 
     try {
       const response = await this.fetch(url.format(target), {
-        method: 'POST',
-        mode: data['sec-fetch-mode'],
-        cache: 'default',
+        method: "POST",
+        mode: data["sec-fetch-mode"],
+        cache: "default",
         body,
         headers,
-      })
-      this.logger.info(`POST ${response.url} - ${response.status}`)
+      });
+      this.logger.info(`POST ${response.url} - ${response.status}`);
     } catch (err) {
-      this.logger.error(err)
+      this.logger.error(err);
     }
   }
 
   onopen() {
-    this.logger.info('Connected', this.events.url)
+    this.logger.info("Connected", this.events.url);
   }
 
   onerror(err: any) {
-    this.logger.error(err)
+    this.logger.error(err);
   }
 
   start() {
     const events = new EventSource(this.source);
 
     // Reconnect immediately
-    (events as any).reconnectInterval = 0 // This isn't a valid property of EventSource
+    (events as any).reconnectInterval = 0; // This isn't a valid property of EventSource
 
-    events.addEventListener('message', this.onmessage.bind(this))
-    events.addEventListener('open', this.onopen.bind(this))
-    events.addEventListener('error', this.onerror.bind(this))
+    events.addEventListener("message", this.onmessage.bind(this));
+    events.addEventListener("open", this.onopen.bind(this));
+    events.addEventListener("error", this.onerror.bind(this));
 
-    this.logger.info(`Forwarding ${this.source} to ${this.target}`)
-    this.events = events
+    this.logger.info(`Forwarding ${this.source} to ${this.target}`);
+    this.events = events;
 
-    return events
+    return events;
   }
 }
 
-export = Client
+export = Client;
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,123 +1,131 @@
-import Client from "../index"
-import { describe, test, expect } from "vitest"
-import { fastify as Fastify } from "fastify"
+import Client from "../index";
+import { describe, test, expect } from "vitest";
+import { fastify as Fastify } from "fastify";
 
-describe('client', () => {
-  describe('createChannel', () => {
-    test('returns a new channel', async () => {
-      const channel = await Client.createChannel()
-      expect(channel).toMatch(/https:\/\/smee\.io\/[0-9a-zA-Z]{10,}/)
-    })
+describe("client", () => {
+  describe("createChannel", () => {
+    test("returns a new channel", async () => {
+      const channel = await Client.createChannel();
+      expect(channel).toMatch(/https:\/\/smee\.io\/[0-9a-zA-Z]{10,}/);
+    });
 
-    test('throws if could not create a new channel', async () => {
-      expect(Client.createChannel({
-        // @ts-ignore
-        fetch: async () => {
-          return {
-            headers: {
-              get: () => null
-            }
-          }
-        }
-      })).rejects.toThrow('Failed to create channel')
-    })
-  })
+    test("throws if could not create a new channel", async () => {
+      expect(
+        Client.createChannel({
+          // @ts-ignore
+          fetch: async () => {
+            return {
+              headers: {
+                get: () => null,
+              },
+            };
+          },
+        }),
+      ).rejects.toThrow("Failed to create channel");
+    });
+  });
 
-  describe('constructor', () => {
-    describe('source', () => {
-      test('throws if source is not a valid URL', () => {
-        expect(() => new Client({
-          source: 'mailto:do-not-reply@example.com',
-          target: 'https://example.com',
-        })
-        ).toThrow('The provided URL is invalid.')
-      })
-    })
-  })
+  describe("constructor", () => {
+    describe("source", () => {
+      test("throws if source is not a valid URL", () => {
+        expect(
+          () =>
+            new Client({
+              source: "mailto:do-not-reply@example.com",
+              target: "https://example.com",
+            }),
+        ).toThrow("The provided URL is invalid.");
+      });
+    });
+  });
 
-  describe('onmessage', () => {
-    test('returns a new channel', async () => {
-      expect.assertions(2)
+  describe("onmessage", () => {
+    test("returns a new channel", async () => {
+      expect.assertions(2);
 
       let finishedPromise = {
         promise: undefined,
         reject: undefined,
         resolve: undefined,
       } as {
-        promise?: Promise<any>,
-        resolve?: (value?: any) => any,
-        reject?: (reason?: any) => any,
-      }
+        promise?: Promise<any>;
+        resolve?: (value?: any) => any;
+        reject?: (reason?: any) => any;
+      };
 
       finishedPromise.promise = new Promise((resolve, reject) => {
-        finishedPromise.resolve = resolve
-        finishedPromise.reject = reject
-      })
+        finishedPromise.resolve = resolve;
+        finishedPromise.reject = reject;
+      });
 
       let callCount = 0;
       const fastify = Fastify();
 
       fastify.route({
-        url: '/',
-        method: 'POST',
+        url: "/",
+        method: "POST",
         handler: async (request, reply) => {
           callCount++;
 
-          expect(JSON.stringify(request.body)).toBe(JSON.stringify({ hello: 'world' }))
+          expect(JSON.stringify(request.body)).toBe(
+            JSON.stringify({ hello: "world" }),
+          );
 
           if (callCount === 2) {
-            finishedPromise.resolve!()
+            finishedPromise.resolve!();
           }
-          return reply.send(request.body).header('content-type', 'application/json')
-        }
-      })
+          return reply
+            .send(request.body)
+            .header("content-type", "application/json");
+        },
+      });
 
-      const target = await fastify.listen()
+      const target = await fastify.listen();
 
-      const source = await Client.createChannel()
+      const source = await Client.createChannel();
       const client = new Client({
         source,
         target,
-      })
+      });
 
       let readyPromise = {
         promise: undefined,
         reject: undefined,
         resolve: undefined,
       } as {
-        promise?: Promise<any>,
-        resolve?: (value?: any) => any,
-        reject?: (reason?: any) => any,
-      }
+        promise?: Promise<any>;
+        resolve?: (value?: any) => any;
+        reject?: (reason?: any) => any;
+      };
 
       readyPromise.promise = new Promise((resolve, reject) => {
-        readyPromise.resolve = resolve
-        readyPromise.reject = reject
-      })
+        readyPromise.resolve = resolve;
+        readyPromise.reject = reject;
+      });
 
-      client.onopen = readyPromise.resolve!
-      client.onerror = readyPromise.reject!
-      client.start()
+      client.onopen = readyPromise.resolve!;
+      client.onerror = readyPromise.reject!;
+      client.start();
 
       await readyPromise.promise;
 
-      await fetch(target + '/', {
-        method: 'POST',
-        body: JSON.stringify({ hello: 'world' }),
+      await fetch(target + "/", {
+        method: "POST",
+        body: JSON.stringify({ hello: "world" }),
         headers: {
-          'content-type': 'application/json',
-        }
-      })
+          "content-type": "application/json",
+        },
+      });
 
       await fetch(source, {
-        method: 'POST',
-        body: JSON.stringify({ hello: 'world' }),
+        method: "POST",
+        body: JSON.stringify({ hello: "world" }),
         headers: {
-          'content-type': 'application/json',
-        }
-      })
+          "content-type": "application/json",
+        },
+      });
 
-      await finishedPromise.promise
-    })
-  })
-})
+      await finishedPromise.promise;
+    });
+  });
+});

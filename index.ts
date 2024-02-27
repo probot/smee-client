60c51f9a34da39fde79abbd81f48afd3eb35f7e2
Yeoh Joer <yeoh.joer@outlook.com>
2024-02-27 19:28:13 +0800

    Terminate the program when the maximum ping difference is exceeded

--- index.ts
+++ index.ts
@@ -8,6 +8,8 @@ type Severity = "info" | "error";
 interface Options {
   source: string;
   target: string;
+  healthcheck: number;
+  maxPingDifference: number;
   logger?: Pick<Console, Severity>;
   fetch?: any;
 }
@@ -15,6 +17,9 @@ interface Options {
 class Client {
   source: string;
   target: string;
+  healthcheck: number;
+  maxPingDifference: number;
+  lastPing: number;
   fetch: typeof global.fetch;
   logger: Pick<Console, Severity>;
   events!: EventSource;
@@ -22,11 +27,16 @@ class Client {
   constructor({
     source,
     target,
+    healthcheck,
+    maxPingDifference,
     logger = console,
     fetch = global.fetch,
   }: Options) {
     this.source = source;
     this.target = target;
+    this.healthcheck = healthcheck;
+    this.maxPingDifference = maxPingDifference;
+    this.lastPing = Date.now();
     this.logger = logger!;
     this.fetch = fetch;
 
@@ -85,6 +95,11 @@ class Client {
     this.logger.info("Connected", this.events.url);
   }
 
+  onping() {
+    this.logger.info(`Received a ping on ${new Date().toISOString()}`);
+    this.lastPing = Date.now();
+  }
+
   onerror(err: any) {
     this.logger.error(err);
   }
@@ -98,6 +113,19 @@ class Client {
     events.addEventListener("message", this.onmessage.bind(this));
     events.addEventListener("open", this.onopen.bind(this));
     events.addEventListener("error", this.onerror.bind(this));
+
+    if (this.healthcheck) {
+      events.addEventListener("ping", this.onping.bind(this));
+
+      setInterval(() => {
+        const difference = (Date.now() - this.lastPing) / 1000;
+
+        if (difference > this.maxPingDifference) {
+          this.logger.error(`Maximum ping difference exceeded. (Difference: ${difference.toFixed(4)}s, Maximum Allowed: ${this.maxPingDifference}s)`);
+          process.exit(1);
+        }
+      }, this.healthcheck * 1000);
+    }
 
     this.logger.info(`Forwarding ${this.source} to ${this.target}`);
     this.events = events;

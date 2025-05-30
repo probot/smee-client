70402e7defefc665cbe0b60853488e61d008f9ec
Aras Abbasi <aras.abbasi@googlemail.com>
2025-05-30 04:05:54 +0200

    more aggressive closing of connections

--- test/smee-server.ts
+++ test/smee-server.ts
@@ -93,6 +93,7 @@ export class SmeeServer {
     this.#emit = undefined;
 
     return new Promise((resolve, reject) => {
+      this.#server.closeAllConnections();
       this.#server.close((err?: Error) => {
         if (err) {
           reject(err);
